bd9fa4727c56ba4e979b7229e4cd0daa90011aa6
Lori Dunford <loridk@gmail.com>
2026-08-25 20:04:37 -0400

    Add footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import Main from "./components/Main";
 import "./App.scss";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       </a>
       <Header />
       <Main />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+function Footer() {
+  return (
+    <footer className="app-footer">
+      <p>
+        Built with <span aria-hidden="true">¯\_(ツ)_/¯</span>
+        <span className="visually-hidden">a shrug</span> by{" "}
+        <a href="https://loridunford.com/">Lori DK</a>.
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
